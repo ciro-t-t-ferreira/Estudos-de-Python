c8ad105ae86f9f849e4d8705490e2aebdc248de5
Ciro Ferreira <124924418+ciro-t-t-ferreira@users.noreply.github.com>
2023-02-28 20:22:04 -0300

    Add files via upload

---
+++ extrator_url.py
@@ -0,0 +1,43 @@
+import re
+
+class ExtratorURL:
+    def __init__(self, url):
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
+
+    def sanitiza_url(self, url):
+        return url.strip()
+
+    def valida_url(self):
+        if self.url == '':
+            raise ValueError("A URL está vazia")
+        
+        padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
+
+    def get_url_base(self):
+        indice_interrogação = self.url.find('?')
+        url_base = self.url[:indice_interrogação]
+        return url_base
+
+    def get_url_parametros(self):
+        indice_interrogação = self.url.find('?')
+        url_parametros = self.url[indice_interrogação + 1:]
+        return url_parametros
+
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e = self.get_url_parametros().find('&', indice_valor)
+        if indice_e == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e]
+        return valor
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+print(valor_quantidade)
